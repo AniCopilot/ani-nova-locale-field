ef1c362aeb68f008c7d91e66ac755f4e4ff1ea07
anikolova <anikolova@eurocoders.com>
2024-12-18 13:02:36 +0200

    Add Bulgarian locale support and enhance Laravel Mix configuration

--- config/nova-locale-field.php
+++ config/nova-locale-field.php
@@ -12,6 +12,7 @@ return [
 
     'locales' => [
         'en' => 'English',
+        'bg' => 'Bulgarian',
     ],
 
     /*
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,10 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix')
+require('./nova.mix')
 
 mix
     .setPublicPath('dist')
     .js('resources/js/field.js', 'js')
-    .vue({version: 3})
-    .webpackConfig({
-        externals: {
-        vue: 'Vue',
-        'laravel-nova': 'LaravelNova'
-        },
-        output: {
-        uniqueName: 'anninikolova/nova-locale-field',
-        },
-
-    })
+    .vue({ version: 3 })
+    .sass('resources/sass/field.scss', 'css')
+    .nova('anni-dineva/nova-locale-field')
+    .version()
